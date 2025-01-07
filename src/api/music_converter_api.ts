1f57221edd9eb05cf463b3a14ecd67fea105bbd4
ivan-astan <astanin.i.a@gmail.com>
2025-01-07 17:46:35 +0300

    api url change

--- src/api/music_converter_api.ts
+++ src/api/music_converter_api.ts
@@ -1,15 +1,10 @@
 import axios from "axios";
 import { Music } from "../state/useMusicStore";
 
-const intenseJSON = axios.create({
-  baseURL: "http://127.0.0.1:8000",
-  headers: {
-    "Content-Type": "application/json",
-  },
+const intense = axios.create({
+  baseURL: "https://musicconverterserver-production.up.railway.app/",
 });
-const intenseFORM = axios.create({
-  baseURL: "http://127.0.0.1:8000",
-});
+
 
 export type ResponseType<D> = {
   error: boolean;
@@ -30,14 +25,14 @@ export type HistoryType = {
 };
 export const ConverterAPI = {
   registerUser: async (name: string, password: string) => {
-    const response = await intenseJSON.post<ResponseType<LoginType>>("/users", {
+    const response = await intense.post<ResponseType<LoginType>>("users", {
       name,
       password,
     });
     return response;
   },
   login: async (name: string, password: string) => {
-    const response = await intenseJSON.post<ResponseType<LoginType>>("/users/login", {
+    const response = await intense.post<ResponseType<LoginType>>("users/login", {
       name,
       password,
     });
@@ -49,21 +44,21 @@ export const ConverterAPI = {
       formData.append(`files`, file);
     });
 
-    const response = await intenseFORM.post<ResponseType<ConvertType>>(
-      `/music/music_converter/${userId}`,
+    const response = await intense.post<ResponseType<ConvertType>>(
+      `music/music_converter/${userId}`,
       formData
     );
     return response.data;
   },
   getHistory: async (userId: number, currentPage: number, pageSize: number) => {
-    const response = await intenseJSON.get<ResponseType<HistoryType>>(
-      `/music/history/${userId}?page=${currentPage}&pageSize=${pageSize}`
+    const response = await intense.get<ResponseType<HistoryType>>(
+      `music/history/${userId}?page=${currentPage}&pageSize=${pageSize}`
     );
     return response.data;
   },
   deleteFile: async (userId: number, fileId: number, pageSize: number = 3) => {
-    const response = await intenseJSON.delete<ResponseType<HistoryType>>(
-      `/music/history/${userId}/${fileId}?pageSize=${pageSize}`
+    const response = await intense.delete<ResponseType<HistoryType>>(
+      `music/history/${userId}/${fileId}?pageSize=${pageSize}`
     );
     return response.data;
   },
